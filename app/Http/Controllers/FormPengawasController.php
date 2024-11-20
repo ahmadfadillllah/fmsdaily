19f4e3703746d6bdc20c48627b7f57a85332ee4b
ahmadfadillllah <ahmadfadillah502@gmail.com>
2024-11-20 11:26:37 +0800

    first

--- app/Http/Controllers/FormPengawasController.php
+++ app/Http/Controllers/FormPengawasController.php
@@ -11,8 +11,75 @@ class FormPengawasController extends Controller
     //
     public function index()
     {
-        // For EX vehicles
-        $ex = [
+        // $ex = DB::connection('sqlsrv')
+        // ->table('FLT_VEHICLE')
+        // ->select([
+        //     'VHC_ID',
+        //     'VHC_TYPEID',
+        //     'VHC_GROUPID',
+        //     'VHC_ACTIVE',
+        // ])
+        // ->where('VHC_ID', 'like', 'EX%')
+        // ->where('VHC_ACTIVE', true)
+        // ->get();
+
+        // $hd = DB::connection('sqlsrv')
+        // ->table('FLT_VEHICLE')
+        // ->select([
+        //     'VHC_ID',
+        //     'VHC_TYPEID',
+        //     'VHC_GROUPID',
+        //     'VHC_ACTIVE',
+        // ])
+        // ->where('VHC_ID', 'like', 'HD%')
+        // ->where('VHC_ACTIVE', true)
+        // ->get();
+
+        // $mg = DB::connection('sqlsrv')
+        // ->table('FLT_VEHICLE')
+        // ->select([
+        //     'VHC_ID',
+        //     'VHC_TYPEID',
+        //     'VHC_GROUPID',
+        //     'VHC_ACTIVE',
+        // ])
+        // ->where('VHC_ID', 'like', 'MG%')
+        // ->where('VHC_ACTIVE', true)
+        // ->get();
+
+        // $bd = DB::connection('sqlsrv')
+        // ->table('FLT_VEHICLE')
+        // ->select([
+        //     'VHC_ID',
+        //     'VHC_TYPEID',
+        //     'VHC_GROUPID',
+        //     'VHC_ACTIVE',
+        // ])
+        // ->where('VHC_ID', 'like', 'BD%')
+        // ->where('VHC_ACTIVE', true)
+        // ->get();
+
+        // $wt = DB::connection('sqlsrv')
+        // ->table('FLT_VEHICLE')
+        // ->select([
+        //     'VHC_ID',
+        //     'VHC_TYPEID',
+        //     'VHC_GROUPID',
+        //     'VHC_ACTIVE',
+        // ])
+        // ->where('VHC_ID', 'like', 'WT%')
+        // ->where('VHC_ACTIVE', true)
+        // ->get();
+
+        // $material = DB::connection('sqlsrv')
+        // ->table('PRD_MATERIAL')
+        // ->select([
+        //     'MAT_ID',
+        //     'MAT_DESC',
+        //     'MAT_DENSITY',
+        // ])->get();
+
+        $ex = collect([
             ['VHC_ID' => 'EX001', 'VHC_TYPEID' => 1, 'VHC_GROUPID' => 1, 'VHC_ACTIVE' => true],
             ['VHC_ID' => 'EX002', 'VHC_TYPEID' => 2, 'VHC_GROUPID' => 1, 'VHC_ACTIVE' => true],
             ['VHC_ID' => 'EX003', 'VHC_TYPEID' => 3, 'VHC_GROUPID' => 2, 'VHC_ACTIVE' => true],
@@ -23,77 +90,39 @@ class FormPengawasController extends Controller
             ['VHC_ID' => 'EX008', 'VHC_TYPEID' => 4, 'VHC_GROUPID' => 1, 'VHC_ACTIVE' => true],
             ['VHC_ID' => 'EX009', 'VHC_TYPEID' => 2, 'VHC_GROUPID' => 4, 'VHC_ACTIVE' => true],
             ['VHC_ID' => 'EX010', 'VHC_TYPEID' => 1, 'VHC_GROUPID' => 2, 'VHC_ACTIVE' => true],
-        ];
+        ]);
 
-        // For HD vehicles
-        $hd = [
+        // Data kendaraan lainnya
+        $hd = collect([
             ['VHC_ID' => 'HD001', 'VHC_TYPEID' => 1, 'VHC_GROUPID' => 2, 'VHC_ACTIVE' => true],
             ['VHC_ID' => 'HD002', 'VHC_TYPEID' => 2, 'VHC_GROUPID' => 3, 'VHC_ACTIVE' => true],
-            ['VHC_ID' => 'HD003', 'VHC_TYPEID' => 3, 'VHC_GROUPID' => 1, 'VHC_ACTIVE' => true],
-            ['VHC_ID' => 'HD004', 'VHC_TYPEID' => 4, 'VHC_GROUPID' => 4, 'VHC_ACTIVE' => true],
-            ['VHC_ID' => 'HD005', 'VHC_TYPEID' => 1, 'VHC_GROUPID' => 3, 'VHC_ACTIVE' => true],
-            ['VHC_ID' => 'HD006', 'VHC_TYPEID' => 3, 'VHC_GROUPID' => 2, 'VHC_ACTIVE' => true],
-            ['VHC_ID' => 'HD007', 'VHC_TYPEID' => 4, 'VHC_GROUPID' => 3, 'VHC_ACTIVE' => true],
-            ['VHC_ID' => 'HD008', 'VHC_TYPEID' => 2, 'VHC_GROUPID' => 1, 'VHC_ACTIVE' => true],
-            ['VHC_ID' => 'HD009', 'VHC_TYPEID' => 3, 'VHC_GROUPID' => 4, 'VHC_ACTIVE' => true],
-            ['VHC_ID' => 'HD010', 'VHC_TYPEID' => 2, 'VHC_GROUPID' => 1, 'VHC_ACTIVE' => true],
-        ];
+            // Tambahkan data lainnya...
+        ]);
 
-        // For MG vehicles
-        $mg = [
+        $mg = collect([
             ['VHC_ID' => 'MG001', 'VHC_TYPEID' => 1, 'VHC_GROUPID' => 1, 'VHC_ACTIVE' => true],
             ['VHC_ID' => 'MG002', 'VHC_TYPEID' => 2, 'VHC_GROUPID' => 3, 'VHC_ACTIVE' => true],
-            ['VHC_ID' => 'MG003', 'VHC_TYPEID' => 3, 'VHC_GROUPID' => 4, 'VHC_ACTIVE' => true],
-            ['VHC_ID' => 'MG004', 'VHC_TYPEID' => 4, 'VHC_GROUPID' => 2, 'VHC_ACTIVE' => true],
-            ['VHC_ID' => 'MG005', 'VHC_TYPEID' => 1, 'VHC_GROUPID' => 1, 'VHC_ACTIVE' => true],
-            ['VHC_ID' => 'MG006', 'VHC_TYPEID' => 2, 'VHC_GROUPID' => 2, 'VHC_ACTIVE' => true],
-            ['VHC_ID' => 'MG007', 'VHC_TYPEID' => 3, 'VHC_GROUPID' => 1, 'VHC_ACTIVE' => true],
-            ['VHC_ID' => 'MG008', 'VHC_TYPEID' => 4, 'VHC_GROUPID' => 3, 'VHC_ACTIVE' => true],
-            ['VHC_ID' => 'MG009', 'VHC_TYPEID' => 1, 'VHC_GROUPID' => 4, 'VHC_ACTIVE' => true],
-            ['VHC_ID' => 'MG010', 'VHC_TYPEID' => 2, 'VHC_GROUPID' => 1, 'VHC_ACTIVE' => true],
-        ];
+            // Tambahkan data lainnya...
+        ]);
 
-        // For BD vehicles
-        $bd = [
+        $bd = collect([
             ['VHC_ID' => 'BD001', 'VHC_TYPEID' => 1, 'VHC_GROUPID' => 3, 'VHC_ACTIVE' => true],
             ['VHC_ID' => 'BD002', 'VHC_TYPEID' => 2, 'VHC_GROUPID' => 4, 'VHC_ACTIVE' => true],
-            ['VHC_ID' => 'BD003', 'VHC_TYPEID' => 3, 'VHC_GROUPID' => 2, 'VHC_ACTIVE' => true],
-            ['VHC_ID' => 'BD004', 'VHC_TYPEID' => 4, 'VHC_GROUPID' => 1, 'VHC_ACTIVE' => true],
-            ['VHC_ID' => 'BD005', 'VHC_TYPEID' => 1, 'VHC_GROUPID' => 2, 'VHC_ACTIVE' => true],
-            ['VHC_ID' => 'BD006', 'VHC_TYPEID' => 2, 'VHC_GROUPID' => 1, 'VHC_ACTIVE' => true],
-            ['VHC_ID' => 'BD007', 'VHC_TYPEID' => 3, 'VHC_GROUPID' => 3, 'VHC_ACTIVE' => true],
-            ['VHC_ID' => 'BD008', 'VHC_TYPEID' => 4, 'VHC_GROUPID' => 2, 'VHC_ACTIVE' => true],
-            ['VHC_ID' => 'BD009', 'VHC_TYPEID' => 1, 'VHC_GROUPID' => 4, 'VHC_ACTIVE' => true],
-            ['VHC_ID' => 'BD010', 'VHC_TYPEID' => 3, 'VHC_GROUPID' => 1, 'VHC_ACTIVE' => true],
-        ];
+            // Tambahkan data lainnya...
+        ]);
 
-        // For WT vehicles
-        $wt = [
+        $wt = collect([
             ['VHC_ID' => 'WT001', 'VHC_TYPEID' => 1, 'VHC_GROUPID' => 4, 'VHC_ACTIVE' => true],
             ['VHC_ID' => 'WT002', 'VHC_TYPEID' => 2, 'VHC_GROUPID' => 1, 'VHC_ACTIVE' => true],
-            ['VHC_ID' => 'WT003', 'VHC_TYPEID' => 3, 'VHC_GROUPID' => 2, 'VHC_ACTIVE' => true],
-            ['VHC_ID' => 'WT004', 'VHC_TYPEID' => 4, 'VHC_GROUPID' => 3, 'VHC_ACTIVE' => true],
-            ['VHC_ID' => 'WT005', 'VHC_TYPEID' => 1, 'VHC_GROUPID' => 1, 'VHC_ACTIVE' => true],
-            ['VHC_ID' => 'WT006', 'VHC_TYPEID' => 3, 'VHC_GROUPID' => 4, 'VHC_ACTIVE' => true],
-            ['VHC_ID' => 'WT007', 'VHC_TYPEID' => 2, 'VHC_GROUPID' => 3, 'VHC_ACTIVE' => true],
-            ['VHC_ID' => 'WT008', 'VHC_TYPEID' => 4, 'VHC_GROUPID' => 1, 'VHC_ACTIVE' => true],
-            ['VHC_ID' => 'WT009', 'VHC_TYPEID' => 1, 'VHC_GROUPID' => 2, 'VHC_ACTIVE' => true],
-            ['VHC_ID' => 'WT010', 'VHC_TYPEID' => 2, 'VHC_GROUPID' => 4, 'VHC_ACTIVE' => true],
-        ];
+            // Tambahkan data lainnya...
+        ]);
 
-        // For materials
-        $material = [
+        $material = collect([
             ['MAT_ID' => 'M001', 'MAT_DESC' => 'Material 1', 'MAT_DENSITY' => 1.25],
             ['MAT_ID' => 'M002', 'MAT_DESC' => 'Material 2', 'MAT_DENSITY' => 1.50],
             ['MAT_ID' => 'M003', 'MAT_DESC' => 'Material 3', 'MAT_DENSITY' => 2.00],
-            ['MAT_ID' => 'M004', 'MAT_DESC' => 'Material 4', 'MAT_DENSITY' => 0.75],
-            ['MAT_ID' => 'M005', 'MAT_DESC' => 'Material 5', 'MAT_DENSITY' => 1.10],
-            ['MAT_ID' => 'M006', 'MAT_DESC' => 'Material 6', 'MAT_DENSITY' => 1.80],
-            ['MAT_ID' => 'M007', 'MAT_DESC' => 'Material 7', 'MAT_DENSITY' => 1.60],
-            ['MAT_ID' => 'M008', 'MAT_DESC' => 'Material 8', 'MAT_DENSITY' => 2.25],
-            ['MAT_ID' => 'M009', 'MAT_DESC' => 'Material 9', 'MAT_DENSITY' => 1.00],
-            ['MAT_ID' => 'M010', 'MAT_DESC' => 'Material 10', 'MAT_DENSITY' => 1.35],
-        ];
+            // Tambahkan data lainnya...
+        ]);
 
         $data = [
             'HD' => $hd,
