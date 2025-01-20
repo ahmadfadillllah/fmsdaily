9cd347569731f7ce71f7579c7a6d7ec0adde82f6
ahmadfadillllah <ahmadfadillah502@gmail.com>
2025-01-20 09:06:02 +0800

    api serviceURL

---
+++ app/Models/FuelServiceURL.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class FuelServiceURL extends Model
+{
+    //
+    protected $table = 'serviceurl_fue_m';
+
+    protected $guarded = [];
+}
